406962f39e3fbcc6d9c0427dca3c2d42d06e365e
Barry Ceelen <barry@multipop.org>
2015-12-14 11:49:03 +0100

    Update plugin header

--- content/mu-plugins/load-example.php
+++ content/mu-plugins/load-example.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Load Must-Use Plugins
 Description: WordPress only looks for PHP files right inside the mu-plugins directory, and (unlike for normal plugins) not for files in subdirectories. Load these plugins by renaming this file to load.php and adding them to the $plugins array.
-Author: Barry Ceelen
+Author: Todo
 Version: 1.0.0
-Author URI: https://github.com/barryceelen
+Author URI: Todo
 */
 
 $plugins = array(
