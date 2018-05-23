ca88b5bea8ef0334c9cf0099d78c33e109e35dcf
Barry Ceelen <barry@multipop.org>
2018-05-23 11:05:46 +0200

    Use chokidar, add Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,8 +186,14 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+	grunt.loadNpmTasks('grunt-chokidar');
+	grunt.registerTask('watch', ['chokidar']);
+
 	grunt.loadNpmTasks( 'grunt-notify' );
 	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-svgmin' );
+
+	grunt.registerTask('default',['sass:dist','autoprefixer','concat:dist','uglify:dist']);
+	grunt.registerTask('dist',['sass:dist','autoprefixer','jshint','concat:dist','uglify:dist','imagemin','svgmin']);
 };
